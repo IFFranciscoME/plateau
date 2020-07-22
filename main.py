bf5ff333319ce32cc14a5131f4028215a0a1bdbd
Francisco ME <if.francisco.me@gmail.com>
2020-07-22 16:54:19 -0500

    New elements added

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: plateau finding technique                                                                  -- #
+# -- file: main.py / principal script for the project                                                    -- #
+# -- author: IFFranciscoME                                                                               -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: https://github.com/IFFranciscoME/plateau                                                -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+
+import pandas as pd
+
+data = pd.read_csv('data/mt4_optimizer.csv')
